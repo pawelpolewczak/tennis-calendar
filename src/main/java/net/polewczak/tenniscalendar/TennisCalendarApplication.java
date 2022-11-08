fb5201e9aa67540ba535a4d85862d0f05ad3697c
Pawel Polewczak <pawel.polewczak@codelab.eu>
2022-11-08 19:37:42 +0100

    Initial commit

---
+++ src/main/java/net/polewczak/tenniscalendar/TennisCalendarApplication.java
@@ -0,0 +1,13 @@
+package net.polewczak.tenniscalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisCalendarApplication.class, args);
+    }
+
+}
